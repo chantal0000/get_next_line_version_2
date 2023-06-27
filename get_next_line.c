aae96cfecd325e3e8ee81be72f94599374992100
Chantal Munja Buerger <c_buerger@gmx.net>
2023-06-27 17:06:04 +0200

    added ft_join and norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,107 +6,112 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 14:08:25 by chbuerge          #+#    #+#             */
-/*   Updated: 2023/06/27 15:16:20 by chbuerge         ###   ########.fr       */
+/*   Updated: 2023/06/27 17:05:24 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <fcntl.h>
 /*Write a function that returns a line read from a
 file descriptor*/
 
 #include "get_next_line.h"
 
-/*
-static void ft_join(char **stash, char *buffer)
+// to join AND FREE the old one
+static void	ft_join(char **stash, char *buffer)
 {
-    char *temp;
+	char	*temp;
 
-    temp = ft_strjoin(*stash, buffer);
-    // loop
-    free(*stash);
-    *stash = temp;
-}*/
+	temp = ft_strjoin(*stash, buffer);
+	free(*stash);
+	*stash = temp;
+}
 
-/*reading lines from the file descriptor, appends (anhaengen) them to the existing
+/*
+reading lines from the file descriptor,
+appends (anhaengen)them to the existing
     1. buffer check...
     2. read data from the file into the buffer,
-    BUFFER_SIZE -> max number of bytes read at a time (returns number of bytes read - >int)
-    3. while bytes_read > 0 meaning, there was a successful read operation
-    4. set last character of the buffer to '\0' to terminate the string in the buffer
+    BUFFER_SIZE -> max number of bytes read at
+	 a time (returns number of bytes read - >int)
+    3. while bytes_read > 0 meaning, there was
+	a successful read operation
+    4. set last character of the buffer to
+	'\0' to terminate the string in the buffer
     5. if !stash call strjoin with "" and buffer
     6. join stash and buffer
     7. search for new line
     8. repeat if not found
     9. finished reading from the file so we can free buffer
 */
-char    *ft_read(int fd, char *stash)
+char	*ft_read(int fd, char *stash)
 {
-    char    *buffer;
-    int     bytes_read_counter;
+	char	*buffer;
+	int		bytes_read_counter;
 
-    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buffer)
-        return (NULL);
-    bytes_read_counter = read(fd, buffer, BUFFER_SIZE);
-    while (bytes_read_counter > 0)
-    {
-        buffer[bytes_read_counter] = '\0';
-        if (!stash)
-            stash = ft_strjoin("", buffer);
-        else
-            //ft_join(&stash, buffer);
-           stash = ft_strjoin(stash, buffer);
-        if (ft_strchr(stash, '\n'))
-            break ;
-        bytes_read_counter = read(fd, buffer, BUFFER_SIZE);
-    }
-    free(buffer);
-    if (bytes_read_counter == -1 && stash != 0)
-    {
-        free(stash);
-        return (NULL);
-    }
-    return (stash);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read_counter = read(fd, buffer, BUFFER_SIZE);
+	while (bytes_read_counter > 0)
+	{
+		buffer[bytes_read_counter] = '\0';
+		if (!stash)
+			stash = ft_strjoin("", buffer);
+		else
+			ft_join(&stash, buffer);
+		if (ft_strchr(stash, '\n'))
+			break ;
+		bytes_read_counter = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+	if (bytes_read_counter == -1 && stash != 0)
+	{
+		free(stash);
+		return (NULL);
+	}
+	return (stash);
 }
 
-/*  1. if fd is < 0 means invalid fd or closed fd
+/*  1. if
+fd is < 0 means invalid fd or closed fd
     2. passing extracted_str as an argument
     to allow the ft_read function to update the
     value of extracted_str based on the data read from the
     file. The function receives the current value of
     static_str and appends the new data read from the
     file to it.
-    3. checks if two conditions are true, a) !extracted_str: checking if the pointer is NULL,
-    meaning check if extracted_str does not point to any valid memory location.
-    b) !*extracted_line: check if the value at the memory location pointed to by extracted_line
-    is NULL meaning checking if the first character of the string extracted_line points to is '\0'
-    -> if this is the case: ....function free_str(&extracted_line) and return NULL
-    4. calls extract line function and passes stash, line from stash is extracted and
+    3. checks if two conditions are true, a) !extracted_str:
+	checking if the pointer is NULL,
+    meaning check if extracted_str does not point to any
+	valid memory location.
+    b) !*extracted_line: check if the value at the memory
+	ocation pointed to by extracted_line
+    is NULL meaning checking if the first character of the s
+	tring extracted_line points to is '\0'
+    -> if this is the case: ....function
+	free_str(&extracted_line) and return NULL
+    4. calls extract line function and passes stash,
+	line from stash is extracted and
     returns it as extracted_line var.
     5. after
 
-TO DO:
-    - ft_new_stat_str
- */
-
-char    *get_next_line(int fd)
+*/
+char	*get_next_line(int fd)
 {
-    static char *stash;
-    char        *extracted_line;
+	static char	*stash;
+	char		*extracted_line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    stash = ft_read(fd, stash);
-    if (!stash || !*stash)
-    {
-        free(stash);
-        stash = NULL;
-        return (NULL);
-    }
-    extracted_line = ft_extract_line(stash);
-    stash = ft_remove_extracted_line_from_stash(stash);
-    return(extracted_line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = ft_read(fd, stash);
+	if (!stash || !*stash)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	extracted_line = ft_extract_line(stash);
+	stash = ft_remove_extracted_line_from_stash(stash);
+	return (extracted_line);
 }
 /*
 RETURN VALUE: Read line: correct behavior
@@ -115,6 +120,8 @@ NULL: there is nothing else to read, or an error
 
 /*test main function*/
 /*
+#include <stdio.h>
+#include <fcntl.h>
 int main(void)
 {
 	char	*temp;
@@ -134,4 +141,3 @@ int main(void)
 	}
 	return (0);
 }*/
-
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,12 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 14:15:47 by chbuerge          #+#    #+#             */
-/*   Updated: 2023/06/27 15:37:47 by chbuerge         ###   ########.fr       */
+/*   Updated: 2023/06/27 17:04:49 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
+
 int	ft_strlen(char *str)
 {
 	int	i;
@@ -26,30 +25,29 @@ int	ft_strlen(char *str)
 }
 
 /*combine the content of stash with the newly read content form fileat*/
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char    *result;
-    int     i;
-    int     j;
+	char	*result;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    result = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!result)
-        return (0);
-    while (s1[i])
-    {
-        result[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        result[i + j] = s2[j];
-        j++;
-    }
-    result[i + j] = '\0';
-    return (result);
-
+	i = 0;
+	j = 0;
+	result = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!result)
+		return (0);
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	result[i + j] = '\0';
+	return (result);
 }
 
 /*finding the new line by strchr*/
@@ -72,39 +70,44 @@ char	*ft_strchr(char *s, int c)
 }
 
 /*
-extracts a single line incl newline char and returns new string
+extracts a single line incl newline char
+and returns new string
 + 2 -> for the \n and nulltermintor
-extracts a line from the stash string by iterating through it until it encounters
-a newline character \n or reaches the end of the string. It then dynamically
-allocates memory for the extracted line and copies the characters from stash
-into new_str. Finally, it adds a newline character \n at the end of new_str before
+extracts a line from the stash string
+by iterating through it until it encounters
+a newline character \n or reaches the end of the string.
+It then dynamically
+allocates memory for the extracted line and
+copies the characters from stash
+into new_str. Finally, it adds a newline character \n
+at the end of new_str before
 returning it.
 */
 
-char    *ft_extract_line(char   *stash)
+char	*ft_extract_line(char *stash)
 {
-    int     i;
-    char    *new_str;
+	int		i;
+	char	*new_str;
 
-    i = 0;
-    while (stash[i] && stash[i] != '\n')
-        i++;
-    new_str = malloc(sizeof(char) * (i + 2));
-    if (!new_str)
-        return (NULL);
-    i = 0;
-    while (stash[i] && stash[i] != '\n')
-    {
-        new_str[i] = stash[i];
-        i++;
-    }
-     if (stash[i] == '\n')
-    {
-        new_str[i] = stash[i];
-        i++;
-    }
-    new_str[i] = '\0';
-    return (new_str);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	new_str = malloc(sizeof(char) * (i + 2));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		new_str[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		new_str[i] = stash[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
 
 /*
@@ -113,7 +116,7 @@ Subtracting i from ft_strlen(stat_str) gives the length of the remaining
 portion of the string after '\n'
 */
 
-char    *ft_remove_extracted_line_from_stash(char *stash)
+char	*ft_remove_extracted_line_from_stash(char *stash)
 {
 	char	*new_str;
 	int		i;
@@ -122,7 +125,7 @@ char    *ft_remove_extracted_line_from_stash(char *stash)
 	i = 0;
 	if (!stash)
 		return (NULL);
-	while(stash[i] && stash[i] != '\n')
+	while (stash[i] && stash[i] != '\n')
 		i++;
 	if (stash[i] == '\0')
 	{
